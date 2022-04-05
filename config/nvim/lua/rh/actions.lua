70861613eb93fe162048a13128e9ba67c31963ea
rkhakov <khakov.ruslan@gmail.com>
2022-04-05 13:39:39 +0500

    Removed print statements

--- config/nvim/lua/rh/actions.lua
+++ config/nvim/lua/rh/actions.lua
@@ -1,11 +1,9 @@
 local M = {}
 
 M.save_and_exec = function()
-  print('foobar')
   if vim.bo.filetype == "vim" then
     vim.cmd 'silent! write'
     vim.cmd 'source %'
-    print('vim')
   elseif vim.bo.filetype == "lua" then
     vim.cmd 'silent! write'
     vim.cmd 'luafile %'

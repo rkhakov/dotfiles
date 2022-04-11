db175440bc48298350c5207423170899d56ea414
rkhakov <khakov.ruslan@gmail.com>
2022-04-11 23:25:58 +0500

    Removed formatting os save

--- config/nvim/after/plugin/null-ls.lua
+++ config/nvim/after/plugin/null-ls.lua
@@ -7,17 +7,17 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
-    null_ls.builtins.diagnostics.flake8
+    null_ls.builtins.diagnostics.flake8,
   },
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd([[
-        augroup LspFormatting
-          autocmd! * <buffer>
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-        augroup END
-      ]])
-    end
-  end,
+  -- on_attach = function(client)
+  --   if client.resolved_capabilities.document_formatting then
+  --     vim.cmd([[
+  --       augroup LspFormatting
+  --         autocmd! * <buffer>
+  --         autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+  --       augroup END
+  --     ]])
+  --   end
+  -- end,
 })
 

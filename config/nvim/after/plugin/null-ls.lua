467296c15a154a6e488258e8fff3f3af38028f9e
rkhakov <khakov.ruslan@gmail.com>
2022-04-20 00:24:36 +0500

    Changed opacity
    * Disabled picom opacity
    * Enabled opacity to kitty
    * Changed i3wm colors

--- config/nvim/after/plugin/null-ls.lua
+++ config/nvim/after/plugin/null-ls.lua
@@ -7,7 +7,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
-    -- null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.flake8,
     -- null_ls.builtins.diagnostics.mypy,
   },
   -- on_attach = function(client)
